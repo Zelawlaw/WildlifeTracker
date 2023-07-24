dd4d0504acd202233b82bf61d62a7b1b514eea8a
Lawrence Karani <lawrencekarani@gmail.com>
2023-07-24 20:25:01 +0300

    initial commit with spring boot initializer

---
+++ src/test/java/com/forestservice/wildlifetracker/WildlifetrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forestservice.wildlifetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WildlifetrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
