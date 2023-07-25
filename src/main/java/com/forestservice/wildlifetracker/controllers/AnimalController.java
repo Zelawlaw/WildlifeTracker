df68d55575109269ff5b0b987d6a7fd499a6c0ff
Lawrence Karani <lawrencekarani@gmail.com>
2023-07-25 20:06:34 +0300

    added postgres properties in application properties file

--- src/main/java/com/forestservice/wildlifetracker/controllers/AnimalController.java
+++ src/main/java/com/forestservice/wildlifetracker/controllers/AnimalController.java
@@ -4,11 +4,7 @@ package com.forestservice.wildlifetracker.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
 
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Controller
 public class AnimalController {
