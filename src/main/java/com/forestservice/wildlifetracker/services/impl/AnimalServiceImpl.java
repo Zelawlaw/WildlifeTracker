eefb6acbe245594b0ce089dec64286fb40501356
Lawrence Karani <lawrencekarani@gmail.com>
2023-07-27 19:29:03 +0300

    modified AnimalExistsError message

--- src/main/java/com/forestservice/wildlifetracker/services/impl/AnimalServiceImpl.java
+++ src/main/java/com/forestservice/wildlifetracker/services/impl/AnimalServiceImpl.java
@@ -61,7 +61,7 @@ public class AnimalServiceImpl implements AnimalService {
     @Override
     public void addEndangeredAnimal(String name, String age, String health) throws AnimalExistsException {
         if (this.animalRepository.findAnimalsByName(name).size() >0 ) {
-            throw new AnimalExistsException("Endangered Animal with name '" + name + "' already exists.");
+            throw new AnimalExistsException("Animal with name '" + name + "' already exists.");
         }
 
         Animal animal = new EndangeredAnimal(name,age,health);
